3143db6a13460546db1d1ddd78dcc27377cc3186
Caleb-Farah <cfarah.dev@gmail.com>
2025-02-03 17:24:14 +0200

    troubleshoot deploy issues

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
